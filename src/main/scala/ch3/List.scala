3598699acca013f2cd6c58ba11466bc6877a0980
Jim DeLois <jimdelois@users.noreply.github.com>
2019-05-25 14:37:54 -0400

    Exercise 3.24 (Pending)

--- src/main/scala/ch3/List.scala
+++ src/main/scala/ch3/List.scala
@@ -266,4 +266,23 @@ object List {
     case (Nil, _) => Nil
     case (Cons(x,xs),Cons(y,ys)) => Cons(f(x,y), zipWith(xs, ys)(f))
   }
+
+  /**
+    * One of the problems with List is that, although we can often express operations and
+    * algorithms in terms of very general-purpose functions, the resulting implementation
+    * isn’t always efficient—we may end up making multiple passes over the same input, or
+    * else have to write explicit recursive loops to allow early termination.
+    *
+    * Exercise 3.24
+    *
+    * Hard: As an example, implement hasSubsequence for checking whether a List contains
+    * another List as a subsequence. For instance, List(1,2,3,4) would have
+    * List(1,2), List(2,3), and List(4) as subsequences, among others. You may have some
+    * difficulty finding a concise purely functional implementation that is also efficient.
+    * That’s okay. Implement the function however comes most naturally. We’ll return to
+    * this implementation in chapter 5 and hopefully improve on it. Note: Any two
+    * values x and y can be compared for equality in Scala using the expression x == y.
+    */
+//  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {}
+
 }
